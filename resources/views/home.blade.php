8262f392bcfdfb2c74cbeef3c147e519293f20d6
raaakib0 <raaakib0@gmail.com>
2025-05-26 22:11:04 +0600

    card update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,14 @@
-
 @extends('shared-layout')
 
 @section('title', 'Home')
 
 @section('content')
 
-<div class="container mt-5">
+    <div class="container mt-5">
         <h2>Surgical Products</h2>
         <div class="row">
             {{-- @dump($products) --}}
-            @foreach($products as $product)
+            @foreach ($products as $product)
                 <div class="col-md-3 mb-4">
                     <div class="card h-100">
                         <img src="{{ $product['image'] }}" class="card-img-top" alt="{{ $product['name'] }}">
@@ -17,9 +16,11 @@
                             <h5 class="card-title">{{ $product['name'] }}</h5>
                             <p class="card-text">{{ $product['description'] }}</p>
                             <p><strong>Size:</strong> {{ $product['size'] }}</p>
-                            <p><strong>Price:</strong> ${{ $product['price'] }}</p>
-                            <p><strong>Brand:</strong> {{ $product['brand'] }}</p>
                             <p><strong>Category:</strong> {{ $product['category'] }}</p>
+                            <div class="d-flex justify-content-between">
+                                <p><strong>Brand:</strong> {{ $product['brand'] }}</p>
+                                <p><strong>Price:</strong> ${{ $product['price'] }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -28,4 +29,3 @@
     </div>
 
 @endsection
-   
